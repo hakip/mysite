9ac2e9e46934a6e20185b6cc41c2e5776a38becc
hakip <randy2995@gmail.com>
2019-12-18 19:21:12 +0700

    Adding homepage and link to login

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    # path('', include('polls.urls')),
+    path('', views.IndexView.as_view(), name='index'),
+    path('account/', include('django.contrib.auth.urls'), name="login"),
     path('admin/', admin.site.urls),
     path('polls', include('polls.urls')),
-    path('', include('blog.urls')),
+    path('blog', include('blog.urls')),
+
 ]
